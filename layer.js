a43114a76b3b71f377878caeef58588cbef59cab
razorsyntax <biomancefilms@hotmail.com>
2016-05-23 15:56:26 -0500

    Added Instance Layer function
    Added Instance Layer function

--- layer.js
+++ layer.js
@@ -1,6 +1,23 @@
 /** 
- * This is the hidden layer where the neurons are stacked
+ * This is the hidden layer where the neuron layers are stacked.
+ * 
+ * Takes an array of instance layers
  */
 
 var HiddenLayer = function(){};
-HiddenLayer.prototype.initialize = function(obj){}
+HiddenLayer.prototype.initialize = function(layersArr){}
+
+/**
+ * Instance of a neuron layer. Create as many as needed
+ * To get started, pass the following layer object to define its properties
+ * {
+ *    layerName: "", // A string name representing layer function, ie. Edge_Detection
+ *    neuronCount: 0, // # of neurons in the layer,
+ *    neurons: [], // the array of neurons to be used in this layer
+ * }
+ */
+
+var InstanceLayer = function(){};
+InstanceLayer.prototype.initialize = function(layerObj){
+  //TODO: This sets up a neuron layer and its properties
+};
