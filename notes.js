9e123f169d8fa8c41fbae15f80932bdb7c22bf38
razorsyntax <biomancefilms@hotmail.com>
2016-05-23 16:24:23 -0500

    Notes file for dev
    Created JSON sketch

---
+++ notes.js
@@ -0,0 +1,37 @@
+// This just personal notes on obj structure and will be deleted once finalized
+
+var neuralNetwork = {
+    inputs: [ //array of input objects
+        { 
+            inputName: "",
+            input: int,
+        }
+    ],
+    hiddenNetwork: {
+        name: ""
+        layers: [// array of layer objects
+            { 
+                layer: "name1",
+                neurons: [
+                    {
+                        id: "1",
+                        activation: tanh"",
+                        weights: [],
+                        bias: []
+                    }
+                ]
+            },
+            {
+                layer: "name2",
+                neurons: [
+                    {
+                        id: "2",
+                        activation: "logsig",
+                        weights: [],
+                        bias: []
+                    }
+                ]
+            }
+        ]
+    }
+}
