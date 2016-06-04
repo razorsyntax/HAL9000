f903d71c6cbe574de71a9a0c686b0625508babfc
razorsyntax <biomancefilms@hotmail.com>
2016-06-04 16:02:12 -0500

    Added sample NN json

--- notes.js
+++ notes.js
@@ -1,54 +1,8 @@
-var NeuralNetwork = {
-        name: "HAL9000",
-        layers: [ // array of layer objects
-            {
-                layer: "Edge_Detection",
-                neurons: [ // array of neuron objects
-                    {
-                        id: "A1",
-                        activation: "tanh",
-                        weights: [],
-                        bias: 1
-                    },
-                    {
-                        id: "A2",
-                        activation: "tanh",
-                        weights: [],
-                        bias: 1
-                    }
-                ]
-            },
-            {
-                layer: "Obj_Detection",
-                neurons: [
-                    {
-                        id: "B1",
-                        activation: "logsig",
-                        weights: [],
-                        bias: 1
-                    }
-                ]
-            }
-        ]
-};
 /**
  * Create separate file for JSON object of the network
  *      * This is used to pre-populate data
  * 
- * *** NFD = null for default
- * Write functions based off this JSON structure
- *    Functions:
- *      LayerFunctions:
- *          Layer(obj or arr or NFD)
- *              -Pass obj with neuron properties for homogeneous layer
- *              -Pass array of neurons
- *              -Leave null for empty layer
- *          add(neuronArr)
- *              -To add neurons to the layer
- *          deleteLayer("layerName")
- *              -Deletes specified layer and all contained neurons
 
- *
  * 
  * 
  *      LearningFunctions:
@@ -59,65 +13,8 @@ var NeuralNetwork = {
  *              -This contains the main thinking loop
  *          StopLearning()
  *              -Stops the thinking loop
- *              
- * 
- * 
- * 
- * 
- * 
- *      Steps to create network:
- *          Method 1)
- *              -Define input data
- *              -Create Neurons
- *              -Add neurons to layer
- *              
- *              -Add layer and inputs to network to create NeuralNetwork JSON object
- * 
- *                Example:
- *                  // define inputs
- *                  var inputs = [1,2,3,4];
- * 
- *                  // Create neurons
- *                  var node1 = new Neuron();
- *                  var node2 = new Neuron({bias:1, activation:"logsig"});
- * 
- *                  // Creates a neuron Arr (checks for type)
- *                  var nodeArr = NeuronArr(node1,node2,...); 
- * 
- *                  // Creates a neuron layer
- *                  var layer = new Layer(nodeArr);
- * 
- *                  // Creates Neural Network Object
- *                  var NN = CreateNetwork(layer); 
- * 
- *                  
- * 
- * 
- *          Method 2)
- *              -Define input data
- *              -Create homogeneous layer
- *              -Add layer and inputs to network to create NeuralNetwork JSON object
- * 
- *                Example:
- *                  // define inputs
- *                  var inputs = [1,2,3,4];
- * 
- *                  // Creates a neuron layer and homogeneous neurons
- *                  var neuronProperties = {
- *                      neuronsToCreate: 2,
- *                      activation: "logsig",
- *                      bias: 1
- *                  };
- * 
- *                  // New layer is created with 2 homogeneous neurons
- *                  var layer = new Layer(neuronProperties);
- * 
- *                  // Creates Neural Network Object
- *                  var NN = CreateNetwork(layer); 
- * 
- * 
- *
- * 
+
+
  *          Train the Network:
  *              Hal.Train(trainingDataArr, NN);
  * 
