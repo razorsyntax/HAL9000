43721638be7cdd0a8bb2a7bab22a66b9dd7e0ce0
razorsyntax <biomancefilms@hotmail.com>
2016-06-10 11:05:29 -0500

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,28 +1,94 @@
-/**
- * First layer receives the input arr
- * Subsequent layers recieve the previous layers output as input
- * 
- * // Layers recieve variable input
- * // Layers output same number of outputs as neurons in that layer
- *      // Layer with two neurons outputs an array of two outputs
- * // Output is used to update the previous weights and bias 
- * 
- * 
- * 
- * 
- * 
- * 
- * Note:
- *      After input data has been processed, the output data is then used to update the weights/bias for all neurons
- * 
- * 
- * 
- *
- * var OutputLayer = function(hiddenLayerArray) {
- * 	//TODO: Determine Gradient Descent to update weights and bias
- * };
-	The goal of backpropagation is to optimize the weights so that the neural network can learn how to correctly map arbitrary inputs to outputs.
-	y = (Σ(wx))+b
-	w is usually a number between -1 & 1 or 0 & 1
-	b = bias starts as 1 and is also weighted as bw (it's own w)
+/*
+Create Network
+	-Create JSON obj
+		-Neurons
+			-Properties
+				-has an activation function
+				-has an id (user defined or system generated)
+			-Actions
+				-Takes inputs, act(SUM(i*w)+b), and outputs the activated value
+			JSON:
+				{"id":123, "activation": "logsig"}
+		-Layers
+			-Properties
+				-has an id (user defined or system generated)
+				-has neurons or not
+				-is an input, hidden, or output layer
+					-Input layer is a vector of inputs
+					-Hidden layer is an array of neurons objs
+					-Output layer is final layer & performs error calculations
+				-JSON:
+					{"id":1, "type": "input",  "items":[]}
+					{"id":2, "type": "hidden", "items":[{}]}
+					{"id":3, "type": "output", "items":[{}]}
+					
+			-Actions
+				-distributes all inputs and outputs as vectors
+				-takes neurons
+				-updates Input layer
+		-Network
+			-Properties
+				-name/id
+				-has layers
+				-has weights/bias obj
+			-Actions
+				-takes inputs and distributes them to layers
+				-Uses target obj to calculate errors
+				-updates weights and bias based on error calculations
+			
+		
+
+
+======================================================
+Methodology:
+
+   Generate WB's       act(SUM(i*w)+b)   
+             |        /|\            |
+            \|/        |            \|/
+Init Input --->   Hidden Layer  ->   Output Layer  |
+            /|\	    							  \|/
+New Input -> |    Update WB's   <- Error Calculation
+
+1) Define Network
+	-Add Neurons
+	-Add them to layers
+2) Define Targets
+3) Set Inputs
+4) Train Network
+	-Generate init WB's for each neuron in all hidden layers
+	-Calculate activated value vector for layer
+		-feedforward in each neuron
+	-Output layer performs error calculations
+	-Update all previous weights/bias at once
+	-Repeat until error is within specified threshold
+
+5) Test Network
+	-Provide Network trained json obj of WB's
+	
+
+
+	
+Tools:
+	Add/Delete/Update Neurons
+	Add/Delete/Update Layers
+	Create initial weights/bias for each input/output from neuron (runs once)
+	Set trained weights/bias for NN
+	Error calculations
+	Update weights/bias
+	
+	
+	
+{
+	"name": "HAL9000",
+	"Layers": {
+		"inputLayer": [],
+		"hiddenLayer":[{}],
+		"outputLayer": [{}]
+	},
+	"targets":[],
+	"wb": {
+		"id1": {"w":.7, "b":1},
+		"id2": {"w":.4, "b":.7}
+	}
+}
 */
