fd8e8d49ab06c4509e3114aaa14ef15fc0e5cb80
razorsyntax <biomancefilms@hotmail.com>
2016-06-15 16:18:59 -0500

    Update HAL9000.js

--- HAL9000.js
+++ HAL9000.js
@@ -9,84 +9,91 @@
 
 //creates NN obj
 var NeuralNetwork = function(name){
-	this.name = name || "NN";
-	this.layers = {
-		"inputLayer":[],
-		"hiddenLayer":[],
-		"outputLayer":[]
-	};
-	this.targets = [];
-	this.wb = {};
+        this.name = name || "NN";
+        this.layers = {
+                "inputLayer":[],
+                "hiddenLayer":[],
+                "outputLayer":[]
+        };
+        this.targets = [];
+        this.wb = {};
 };
 
 //creates new layer
 NeuralNetwork.prototype.createLayer = function(attr){
-	var layerID;
-	var layerCount = this.layers.hiddenLayer.length;
+        var layerID;
 
-	if(layerCount < 1){
-		layerID = "A";
-	} else{
-		layerID = utilities.nextletter(layerCount);
-	}
-
-	if(attr.neurons.length > 0){
-		for(let i=0;i<attr.neurons.length;i++){
-			attr.neurons[i].id = layerID + (i + 1).toString();
-		}
-	}
-
-	this.layers.hiddenLayer.push({
-		name: attr.name || "",
-		layerID: layerID,
-		type: attr.type,
-		neurons: attr.neurons,
-	});
+        switch(attr.type){
+            case "hidden":
+            	var layerCount = this.layers.hiddenLayer.length;
+            	layerID = (layerCount < 1) ? "A": utilities.nextletter(layerCount);
+	
+            	if(attr.neurons.length > 0){
+        	    for(let i=0;i<attr.neurons.length;i++){
+        	        attr.neurons[i].id = layerID + (i + 1).toString();
+        	    }
+            	}
+	
+            	this.layers.hiddenLayer.push({
+        	    name: attr.name || "",
+        	    layerID: layerID,
+        	    type: attr.type,
+        	    neurons: attr.neurons,
+            	});
+            	break;
+            case "output":
+                this.layers.outputLayer = {
+                    type: attr.type,
+                    neurons: attr.neurons,
+                }
+                break;
+        }
 };
 
 //sets training targets
 NeuralNetwork.prototype.setTarget = function(targets){
-	this.targets = targets;
+    this.targets = targets;
 };
 
 
 
 NeuralNetwork.prototype.init = function(numInputs){
-	var keys = [];
-	var wb = {};
-	var w = [];
-	var b = 0;
-	var num;
-	var obj = {};
-	var arr = this.layers.hiddenLayer;
-	for(let i=0;i<arr.length;i++){
-		var neurons = arr[i].neurons;
-		for(let i=0;i<neurons.length;i++){
-			keys.push(neurons[i].id);
-		}
-	}
-	for(let i=0;i<keys.length;i++){
-		for(let j=0;j<numInputs.length;j++) {
-			num = NeuralMathLib.randomGauss();
-			w.push(num);
-		}
-		num = NeuralMathLib.randomGauss();
-		obj = {"w": w, "b": num};
-		wb[keys[i]] = obj;
-		w = [];
-	}
-	this.wb = wb;
+    var keys = [];
+    var wb = {};
+    var w = [];
+    var b = 0;
+    var num;
+    var obj = {};
+    var hArr = this.layers.hiddenLayer;
+    var oArr = this.layers.outputLayer.neurons;
+    for(let i=0;i<hArr.length;i++){
+        var neurons = hArr[i].neurons;
+        for(let i=0;i<neurons.length;i++){
+            keys.push(neurons[i].id);
+        }
+    }
+    for(let i=0;i<keys.length;i++){
+        for(let j=0;j<numInputs.length;j++) {
+            num = NeuralMathLib.randomGauss();
+            w.push(num);
+        }
+        num = NeuralMathLib.randomGauss();
+        obj = {"w": w, "b": num};
+        wb[keys[i]] = obj;
+        w = [];
+    }
+
+	
+
+    this.wb = wb;
 };
 
 
+var Train = function(NN, inputs){
 
 
-
-
-
-NeuralNetwork.prototype.Train = function(inputs, errorOBJ) {
-	//counts number of inputs
-	//sets weights and biases with SetWB();
+    NN.init(inputs); // setting hidden input wb's
+    debugger;
 	
 	//Calculate outputs for all layers
 		//Once to the output layers
@@ -94,15 +101,10 @@ NeuralNetwork.prototype.Train = function(inputs, errorOBJ) {
 		//Change the output layer weights
 	//Calculate hidden layer errors (back prop)
 		//Change hidden layer weights
-	
-}
-
-
-
-var Train = function(trainingDataArr, NN){};
+};
 
 var Learn = function(inputArr, NN){};
 
 var StopLearning = function(){
-	return false;
+    return false;
 };
