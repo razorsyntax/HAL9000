2f3455b8acc91de92f6f4fde4c5bd359c153117c
razorsyntax <biomancefilms@hotmail.com>
2016-09-25 08:10:24 -0500

    Consolidated js files into one

--- notes.js
+++ notes.js
@@ -1,52 +1,15 @@
 /*
-Create Network
-	-Create JSON obj
-		-Neurons
-			-Properties
-				-has an activation function
-				-has an id (user defined or system generated)
-			-Actions
-				-Takes inputs, act(SUM(i*w)+b), and outputs the activated value
-			JSON:
-				{"id":123, "activation": "logsig"}
-		-Layers
-			-Properties
-				-has an id (user defined or system generated)
-				-has neurons or not
-				-is an input, hidden, or output layer
-					-Input layer is a vector of inputs
-					-Hidden layer is an array of neurons objs
-					-Output layer is final layer & performs error calculations
-				-JSON:
-					{"id":1, "type": "input",  "neurons":[]}
-					{"name":"custName", "id":2, "type": "hidden", "neurons":[{}]}
-					{"id":3, "type": "output", "neurons":[{}]}
-					
-			-Actions
-				-distributes all inputs and outputs as vectors
-				-takes neurons
-				-updates Input layer
-		-Network
-			-Properties
-				-name/id
-				-has layers
-				-has weights/bias obj
-			-Actions
-				-takes inputs and distributes them to layers
-				-Uses target obj to calculate errors
-				-updates weights and bias based on error calculations
-			
-		
 
+A scratch pad for thoughts and implementation.
 
-======================================================
 Methodology:
 
    Generate WB's       act(SUM(i*w)+b)   
              |        /|\            |
             \|/        |            \|/
-Init Input --->   Hidden Layer  ->   Output Layer  |
-            /|\	    				  \|/
+Initial Input --->   Hidden Layer --->  Output Layer  
+		   	/|\						 |
+             |	    				\|/
 New Input -> |    Update WB's   <- Error Calculation
 
 1) Define Network
@@ -64,93 +27,4 @@ New Input -> |    Update WB's   <- Error Calculation
 
 5) Test Network
 	-Provide Network trained json obj of WB's
-	
-======================================================
-
-	
-Tools:
-	Add/Delete/Update Neurons
-	Add/Delete/Update Layers
-	Create initial weights/bias for each input/output from neuron (runs once)
-	Set trained weights/bias for NN
-	Error calculations
-	Update weights/bias
-
-
-//creates instance of NN obj
-var NN = new NeuralNetwork();
-
-//create neurons with activation attribute
-var node = new Neuron({
-	"activation":"logsig"
-});
-
-var node2 = new Neuron({
-	"activation":"logsig"
-});
-
-//create layer and give it attributes
-NN.createLayer({
-	"name": "Edge_Detection",
-	"neurons": [node,node2]
-});
-
-//set targets
-var targetArr = [1,0,1];
-NN.targets(targetArr);
-
-//this creates a JSON obj with all initial weights and biases in NN
-NN.initWB();
-
-
-//NN is now set for work
-
-var inputs = [1,1,1];
-
-//send the inputs through your network
-//you may also specify optional error threshold obj for when training should end 
-NN.Train(inputs, {"error":[0.6,0.7,0.5]});
-
-//test your network with new input data
-
-var testInput = [0,0,1];
-
-NN.Test(testInput);
-
-
-
-
-
-==============================================
-
-//creates instance of NN obj
-var NN = new NeuralNetwork("HAL9000");
-
-
-
-
-
-
-
-//creates single neuron instance
-var Neuron = function(activation){
-	this.activation = activation || "logsig"
-};
-
-
-var hiddenLayerCal = function(NN){
-	var layers = NN.layers;
-	var wbs = NN.wb;
-	for(let i=0;i<layers.length;i++){
-		var neurons = layers[i].neurons;
-		for(let j=0;j<neurons.length;j++){
-			//sum weights and biases neurons[j];
-			
-		}
-	}
-}
-
-
-
-
 */
